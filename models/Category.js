473e66a460142ba0b9b46b391341a831fe809e3c
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-05-17 12:11:13 -0600

    associated models within model/index.js, and defined the columns within the models

--- models/Category.js
+++ models/Category.js
@@ -1,26 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+/** @format */
 
-const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
 module.exports = Category;
-
-
-
 
 
 // use the key word .belongsto within activity 21
